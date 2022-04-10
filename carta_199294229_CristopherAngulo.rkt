8eab8037d25e8cf16b8fbeb8028d1877f3ef002b
AndromedaK2 <102329976+AndromedaK2@users.noreply.github.com>
2022-04-10 00:09:57 -0400

    gitIgnore
    [add] gitIgnore

--- carta_199294229_CristopherAngulo.rkt
+++ carta_199294229_CristopherAngulo.rkt
@@ -1,4 +1,22 @@
 #lang scheme
+(require "simbolo_199294229_CristopherAngulo.rkt")
 ;Implementación del TDA carta
-;Representación:
+;Representación: Lista de Simbolos
+
+;Constructor
+;Dominio: Lista
+;Recorrido null | Lista
+(define createCard (lambda( list length )
+                     (if (null? list)  
+                         null 
+                         list
+                         )))
+
+;Pertenencia
+;Dominio: Lista
+;Recorrido True o False
+(define isValidCard(lambda (list lenght)
+                     (if (null? list)
+                         #true
+                         #false)))
 
