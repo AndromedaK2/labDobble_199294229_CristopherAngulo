96e2e00b24c851e830949aa19582949b38edec94
AndromedaK2 <102329976+AndromedaK2@users.noreply.github.com>
2022-04-11 17:58:18 -0400

    validate order
    [add] validation of order and [add] validate prime number

--- mazo_199294229_CristopherAngulo.rkt
+++ mazo_199294229_CristopherAngulo.rkt
@@ -1,22 +1,40 @@
 #lang scheme
 (provide cardsSet)
 ;Implementación del TDA mazo
-;Representación:  lista de Cartas
+;Representación:  Lista de Cartas
 
 ;Constructor
 ;Dominio: Elements (list) X numE(int) X maxC(int) X rndFn (fn)
 ;Recorrido: CardsSet
+;Descripción: 
 (define cardsSet (lambda (Elements numE maxC rndFn)
-                   (if (and (>= numE 1)(>= maxC 1)
-                            (and (not(null? Elements))(list? Elements)))
-                       null
-                       null)))
+     (if (and (>= numE 1)(>= maxC 1) (isValidOrder (- numE 1)) (not(null? Elements)))
+          null
+          null
+     ))
+)
+
+
+; numE - 1 -> orden del plano proyectivo
+; maxC
+
+(define isValidOrder(lambda (order)
+     (if (isPrimeWrapper order)
+         #true
+         #false 
+     ))
+)
+
+(define isPrimeWrapper (lambda (number)    
+ (isPrime number 2)
+))
+
+(define isPrime ( lambda (number count)
+     (if (= number count) #t
+     (if (= (remainder number count) 0) #f
+     (isPrime number (+ count 1))))))
 
 
 
-
-
-
-
-
+; ejemplo de uso (isPrimeWrapper 4)
 
