e9ff41c9d9168b52e8dc1ee853c0fd30f25ebd0f
AndromedaK2 <102329976+AndromedaK2@users.noreply.github.com>
2022-04-23 00:07:42 -0400

    cardsSet to string
    [add] cardsSet to string

--- carta_19929422_CristopherAngulo.rkt
+++ carta_19929422_CristopherAngulo.rkt
@@ -1,6 +1,6 @@
 #lang scheme
 (require "simbolo_19929422_CristopherAngulo.rkt")
-(provide createCard getlengthCard cards? )
+(provide createCard getlengthCard cards? card->string )
 ;Implementación del TDA carta
 ;Representación: Lista de Simbolos
 
@@ -19,9 +19,49 @@
                      #true
                      #false)))
 
+;Pertenencia
+;Dominio: Carta
+;Recorrido: True | False
+;Descripción: Retorna verdadero si el valor entrante es una carta sino retorna falso
+;(define card? (lambda (card)
+;  (and (list? card)
+;     (not (null? card))
+;    (not (null? (producto (car p) (cdr p))))))              
+
 ;Selector
 ;Dominio:  Carta
 ;Recorrido:  Largo
 ;Descripción: Retorna el largo de la carta 
 (define getlengthCard (lambda (card) (length card)))
+
+
+;Selector
+;Dominio:  Carta
+;Recorrido:  Largo
+;Descripción: Retorna el primer simbolo de la carta
+(define getFirstElementOfCard (lambda (card) (car card)))
+
+;Selector
+;Dominio:  Carta
+;Recorrido:  Carta 
+;Descripción: Retorna carta sin el primer elemento o simbolo
+(define getLastElementsOfCard (lambda (card) (cdr card)))
+
+
+;Modificador: Carta
+;Recorrido: String
+;Descripción: Retorna la carta en formato de string
+(define card->string (lambda (card position)
+   (define card->stringAuxiliar (lambda (card cardToString)
+    (if (null? card)
+       cardToString
+       (card->stringAuxiliar (getLastElementsOfCard card) (string-append cardToString " " (number->string (getFirstElementOfCard card)))) 
+     )))
+  (card->stringAuxiliar card (string-append "C" (number->string position) ":"))))
+
+
+;Ejemplo
+(card->string (list 2 3 4) 1)
+
+        
                  
--- mazo_19929422_CristopherAngulo.rkt
+++ mazo_19929422_CristopherAngulo.rkt
@@ -378,11 +378,33 @@
                             n countTotalCards (+ i 1)))))  
 
 ;Elementos de ejemplo
+;(define elements (list  1 2 3 4 5 6 7 8 9 10 11 12 13))
+;(define elementoss (list (element "A") (element 2) (element "D") (element "C") (element "3") (element 8) (element 10)))
 (define elements (list  1 2 3 4 5 6 7 8 9 10 11 12 13))
 (define elementoss (list (element "A") (element 2) (element "D") (element "C") (element "3") (element 8) (element 10)))
-
-
-
-;(outerCardsSet '((5 6) (8 2) (3 4) (7 2)) '(( 5 6) (3 4))) ; ==> (3 4)
-
+;Mazo de carta de ejemplo
+(cardsSet elementoss 3 -1 3)
+(cardsSet elementoss 3 7 3)
+(cardsSet elements   4 13 3)
+(cardsSet elements   4 10 3)
+;Cantidad de cartas del mazo
+(numCards (cardsSet elementoss 3 7 3) )
+(numCards (cardsSet elementoss 3 7 3) )
+;Retornar Carta
+(nthCard (cardsSet elements  4 13 3) 2 )
+(nthCard (cardsSet elements   4 13 3) 2 )
+;Encontrar total de cartas
+(findTotalCards (nthCard (cardsSet elements 4 13 3) 2) )
+(findTotalCards (nthCard (cardsSet elementoss 3 7 3) 2) )
+;el juego es valido dobble
+;(dobble? (cardsSet elements 4 13 3)) 
+;(define elementoss (list (element "A") (element 2) (element "D") (element "C") (element "3") (element 8) (element 10)))
+(dobble? (cardsSet elements 4 13 3)) 
 (missingCards (cardsSet elementoss 3 5 3) elementoss) 
+
+(display (cardsSet->string (cardsSet elements 4 13 3)))  
+
+
+
+
+ 
