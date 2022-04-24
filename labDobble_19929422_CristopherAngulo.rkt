24d459547fd6a9c02eec89e427dc39eea152f16d
AndromedaK2 <102329976+AndromedaK2@users.noreply.github.com>
2022-04-24 00:07:36 -0400

    add examples
    [Add] examples

--- labDobble_19929422_CristopherAngulo.rkt
+++ labDobble_19929422_CristopherAngulo.rkt
@@ -5,49 +5,46 @@
 (require "jugador_19929422_CristopherAngulo.rkt")
 
 
-; (define m 22)
-; (define a 15)
-; (define c 38)
+(define m 22)
+(define a 15)
+(define c 38)
 
-; (define randomFn (lambda (xn)(modulo (+ (* a xn) c) m)))
+(define randomFn (lambda (xn)(modulo (+ (* a xn) c) m)))
 
-; ;Elementos de ejemplo
-; ;(define elements (list  1 2 3 4 5 6 7 8 9 10 11 12 13))
-; ;(define elementoss (list (element "A") (element 2) (element "D") (element "C") (element "3") (element 8) (element 10)))
-; (define elements (list  1 2 3 4 5 6 7 8 9 10 11 12 13))
-; (define elementoss (list (element "A") (element 2) (element "D") (element "C") (element "3") (element 8) (element 10)))
-; ;Mazo de carta de ejemplo
-; (cardsSet elementoss 3 -1 randomFn)
-; (cardsSet elementoss 3 7  randomFn)
-; (cardsSet elements   4 13 randomFn)
-; (cardsSet elements   4 10 randomFn)
-; ;Cantidad de cartas del mazo
-; (numCards (cardsSet elementoss 3 6 randomFn) )
-; (numCards (cardsSet elementoss 3 7 randomFn) )
-; (numCards (cardsSet elementoss 3 5 randomFn) )
-; ;Retornar Carta
-; (nthCard (cardsSet elements  3 7 randomFn) 2 )
-; (nthCard (cardsSet elements  4 13 randomFn) 3 )
-; (nthCard (cardsSet elements  4 10 randomFn) 1 )
-; ;Encontrar total de cartas
-; (findTotalCards (nthCard (cardsSet elements 4 13 randomFn) 2) )
-; (findTotalCards (nthCard (cardsSet elementoss 3 7 randomFn) 2) )
-; (findTotalCards (nthCard (cardsSet elementoss 3 6 randomFn) 2) )
-; ;el juego es valido dobble
-; ;(dobble? (cardsSet elements 4 13 3)) 
-; ;(dobble? (cardsSet elements 4 10 3)) 
-; ;(dobble? (cardsSet elements 3 7 3)) 
-
-
-; ;(define elementoss (list (element "A") (element 2) (element "D") (element "C") (element "3") (element 8) (element 10)))
-; (dobble? (cardsSet elements 4 13 randomFn)) 
-; (missingCards (cardsSet elementoss 3 5 randomFn) elementoss randomFn) 
-; (missingCards (cardsSet elementoss 3 6 randomFn) elementoss randomFn) 
-; (missingCards (cardsSet elementoss 3 7 randomFn) elementoss randomFn) 
-
-; (display (cardsSet->string (cardsSet elements 4 13 randomFn)))
-; (display (cardsSet->string (cardsSet elements 3 7 randomFn)))  
-; (display (cardsSet->string (cardsSet elements 3 5 randomFn)))  
+;Elementos de ejemplo
+;(define elements (list  1 2 3 4 5 6 7 8 9 10 11 12 13))
+;(define elementoss (list (element "A") (element 2) (element "D") (element "C") (element "3") (element 8) (element 10)))
+(define elements (list  1 2 3 4 5 6 7 8 9 10 11 12 13))
+(define elementoss (list (element "A") (element 2) (element "D") (element "C") (element "3") (element 8) (element 10)))
+;Mazo de carta de ejemplo
+(cardsSet elementoss 3 -1 randomFn)
+(cardsSet elementoss 3 7  randomFn)
+(cardsSet elements   4 13 randomFn)
+(cardsSet elements   4 10 randomFn)
+;Cantidad de cartas del mazo
+(numCards (cardsSet elementoss 3 6 randomFn) )
+(numCards (cardsSet elementoss 3 7 randomFn) )
+(numCards (cardsSet elementoss 3 5 randomFn) )
+;Retornar Carta
+(nthCard (cardsSet elements  3 7 randomFn) 2 )
+(nthCard (cardsSet elements  4 13 randomFn) 3 )
+(nthCard (cardsSet elements  4 10 randomFn) 1 )
+;Encontrar total de cartas
+(findTotalCards (nthCard (cardsSet elements 4 13 randomFn) 2) )
+(findTotalCards (nthCard (cardsSet elementoss 3 7 randomFn) 2) )
+(findTotalCards (nthCard (cardsSet elementoss 3 6 randomFn) 2) )
+;el juego es valido dobble
+(dobble? (cardsSet elements 4 13 randomFn)) 
+(dobble? (cardsSet elements 3 7 randomFn)) 
+(dobble? (cardsSet elements 2 3 randomFn)) 
+;Cartas Faltantes
+(missingCards (cardsSet elementoss 3 5 randomFn) elementoss randomFn) 
+(missingCards (cardsSet elementoss 3 6 randomFn) elementoss randomFn) 
+(missingCards (cardsSet elementoss 3 7 randomFn) elementoss randomFn) 
+;Mostrar las cartas del mazo
+(display (cardsSet->string (cardsSet elements 4 13 randomFn)))
+(display (cardsSet->string (cardsSet elements 3 7 randomFn)))  
+(display (cardsSet->string (cardsSet elements 3 5 randomFn)))  
 
 
 
